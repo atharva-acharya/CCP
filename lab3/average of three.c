cb908f9ebc659cd7a1295c2cc5b5ad0f0fa6ad5e
atharva-acharya <83897474+atharva-acharya@users.noreply.github.com>
2021-05-24 09:14:24 +0530

    Add files via upload

---
+++ lab3/average of three.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+  float a,b,c,average;
+
+  printf("\nPlease Enter three nos. :\n");
+  scanf("%f%f%f",&a,&b,&c);
+
+  average =(a+b+c)/2;
+  printf("\n average of the three is = %f\n",average);
+
+  return 0;
+}
