637de3230db8c71270cbf94950527ceec6204ec9
atharva-acharya <83897474+atharva-acharya@users.noreply.github.com>
2021-05-26 10:08:29 +0530

    Add files via upload

---
+++ homework problems/multiplication.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,i,multi;
+    printf("Enter two Numbers");
+    scanf("%d %d",&a,&i);
+    multi=a*i;
+    printf("Multiplication of two floating Numbers %d and %d is %d",a,i,multi);
+    return 0;
+    getch();
+}
+
